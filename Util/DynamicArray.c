9ab228db4043c8050150dc729049256aef90a513
7Demons <robert.cresswellnz@gmail.com>
2023-10-16 22:13:43 +1300

    added swap

--- Util/DynamicArray.c
+++ Util/DynamicArray.c
@@ -6,6 +6,13 @@
 #define TRUE 1
 #define FALSE 0
 
+/** TODO:
+ * * many function assume data is not NULL. should be fine.
+ * * free function.
+ * 
+ * 
+*/
+
 typedef struct DynamicArray {
     void *data;
     int elementSize;
@@ -68,11 +75,87 @@ BOOL push(DynamicArray *pArray, void *value) {
     return TRUE;
 }
 
+BOOL pop(DynamicArray *pArray) {
+    if (pArray == NULL) {
+        return FALSE;
+    }
+    if (pArray->data == NULL) {
+        return FALSE;
+    }
+    if (pArray->size <= 0) {
+        return FALSE;
+    }
+    //void *deleteAddress = (char *)pArray->data + (pArray->elementSize * pArray->size);
+    //free(deleteAddress);
+    pArray->size--;
+    
+    if (pArray->size < pArray->maxCapacity / 2) {
+        int newNoElem = pArray->maxCapacity/2;
+        int newSizeBytes = newNoElem * pArray->elementSize;
+        void *temp = (void *)realloc(pArray->data, newSizeBytes);
+        if (temp == NULL) {
+            printf("Error: Reallocation failure!");
+            return FALSE;
+        }
+        pArray->data = temp;
+        pArray->maxCapacity = newNoElem;
+    }
+
+    return TRUE;
+}
+
+BOOL swap(DynamicArray *pArray, int indexP, int indexQ) {
+    if (pArray == NULL) {
+        return FALSE;
+    }
+    if (pArray->data == NULL) {
+        return FALSE;
+    }
+    if (indexP < 0 || indexQ < 0) {
+        return FALSE;
+    }
+    if (pArray->size < indexP || pArray->size < indexQ) {
+        return FALSE;
+    }
+
+    void *temp = malloc(pArray->elementSize);
+    if (temp == NULL) {
+        return FALSE;
+    }
+
+    void *pIndexP = (char*)pArray->data + (pArray->elementSize * indexP);
+    void *pIndexQ = (char*)pArray->data + (pArray->elementSize * indexQ);
+    memcpy(temp, pIndexP, pArray->elementSize);
+    memcpy(pIndexP, pIndexQ, pArray->elementSize);
+    memcpy(pIndexQ, temp, pArray->elementSize);
+    free(temp);
+}
+
+void *get(DynamicArray *pArray, int index) {
+    if (pArray == NULL) {
+        return NULL;
+    }
+    if (pArray->size < index) {
+        return NULL;
+    }
+    void *address = (char *)pArray->data + (pArray->elementSize * index);
+    return address;
+}
+
 void debugArray(DynamicArray *pArray) {
-    int *pData = pArray->data;
+    // int *pData = pArray->data;
+    // for (int i = 0; i < pArray->size; i++) {
+    //     printf("%d\n", *pData);
+    //     pData++;
+    // }
+
     for (int i = 0; i < pArray->size; i++) {
-        printf("%d\n", *pData);
-        pData++;
+        int *val = get(pArray, i);
+        if (val == NULL) {
+            break;
+        }
+        printf("[index %d]: %d\n", i, *val);
+        
     }
 
 }
@@ -86,17 +169,29 @@ int main() {
     int x = 55555;
     int y = 7;
     int z = 4;
-    push(test, &x);
-    push(test, &y);
-    push(test, &z);
 
-
-    for (int i = 0; i < 80000000; i++) {
+    for (int i = 0; i < 3; i++) {
         push(test, &x);
         push(test, &y);
+        push(test, &z);
     }
 
-    //debugArray(test);
+    debugArray(test);
+
+    printf("\n\n");
+    pop(test);
+    pop(test);
+    pop(test);
+    pop(test);
+    pop(test);
+    pop(test);
+    pop(test);
+    debugArray(test);
+
+    printf("\n\n");
+    swap(test, 0, 1);
+    debugArray(test);
+
     printf("%d \n", test->maxCapacity);
     const int a; 
     scanf("%d", &a);

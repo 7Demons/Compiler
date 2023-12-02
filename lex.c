d736771f06faf0f6ccaf77c237c301613894f472
7Demons <robert.cresswellnz@gmail.com>
2023-12-02 23:26:37 +1300

    lex msvc

--- lex.c
+++ lex.c
@@ -1,205 +1,323 @@
-#include "lex.h"
+#ifndef LEX_H_
+#define LEX_H_
 
-BOOL isOperator(char value) {
-    switch(value) {
-        case '+':
-        case '-':
-        case '*':
-        case '/':
-        case '%':
-        case '=':
-            return TRUE;
-            break;
-    }
-    return FALSE;
-}
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include "readFile.c"
+#include "Util/DynamicArray.c"
 
-BOOL isSeperator(char value) {
-    switch(value) {
-        case '(':
-        case ')':
-        case '\'':
-        case '\"':
-        case '{':
-        case '}':
-        case ';':
-        case ',':
-            return TRUE;
-            break;
-    }
-    return FALSE;
-}
+typedef int boolean; 
 
-Keyword getKeyword(span_t *fSpan) {
-    size_t windowSize = fSpan->right - fSpan->left;
-    char *charWindow = malloc(sizeof(char)*windowSize);
-    memcpy(charWindow, fSpan->left, windowSize);
+// As defined in the microsoft visual studio C language reference 2022.
+// https://learn.microsoft.com/en-us/cpp/c-language/lexical-grammar?view=msvc-170
+// Some of the definitions have been codified into their respective structure here.
+// others will be done during lexing.
+//
 
-    Keyword result = 0;
-    // this is silly and wasteful.
-    result += (memcmp(charWindow, &"auto", sizeof(char)*4) == 0) ? _auto : 0;
-    result += (memcmp(charWindow, &"break", sizeof(char)*5) == 0) ? _break : 0;
-    result += (memcmp(charWindow, &"case", sizeof(char)*4) == 0) ? _case : 0;
-    result += (memcmp(charWindow, &"char", sizeof(char)*4) == 0) ? _char : 0;
-    result += (memcmp(charWindow, &"const", sizeof(char)*5) == 0) ? _const : 0;
-    result += (memcmp(charWindow, &"continue", sizeof(char)*8) == 0) ? _continue : 0;
-    result += (memcmp(charWindow, &"default", sizeof(char)*7) == 0) ? _default : 0;
-    result += (memcmp(charWindow, &"do", sizeof(char)*2) == 0) ? _do : 0;
-    result += (memcmp(charWindow, &"double", sizeof(char)*6) == 0) ? _double : 0;
-    result += (memcmp(charWindow, &"else", sizeof(char)*4) == 0) ? _else : 0;
-    result += (memcmp(charWindow, &"enum", sizeof(char)*4) == 0) ? _enum : 0;
-    result += (memcmp(charWindow, &"extern", sizeof(char)*6) == 0) ? _extern : 0;
-    result += (memcmp(charWindow, &"float", sizeof(char)*5) == 0) ? _float : 0;
-    result += (memcmp(charWindow, &"for", sizeof(char)*3) == 0) ? _for : 0;
-    result += (memcmp(charWindow, &"goto", sizeof(char)*4) == 0) ? _goto : 0;
-    result += (memcmp(charWindow, &"if", sizeof(char)*2) == 0) ? _if : 0;
-    result += (memcmp(charWindow, &"int", sizeof(char)*3) == 0) ? _int : 0;
-    result += (memcmp(charWindow, &"long", sizeof(char)*4) == 0) ? _long : 0;
-    result += (memcmp(charWindow, &"register", sizeof(char)*8) == 0) ? _register : 0;
-    result += (memcmp(charWindow, &"return", sizeof(char)*6) == 0) ? _return : 0;
-    result += (memcmp(charWindow, &"short", sizeof(char)*5) == 0) ? _short : 0;
-    result += (memcmp(charWindow, &"signed", sizeof(char)*6) == 0) ? _signed : 0;
-    result += (memcmp(charWindow, &"sizeof", sizeof(char)*6) == 0) ? _sizeof : 0;
-    result += (memcmp(charWindow, &"static", sizeof(char)*6) == 0) ? _static : 0;
-    result += (memcmp(charWindow, &"struct", sizeof(char)*6) == 0) ? _struct : 0;
-    result += (memcmp(charWindow, &"switch", sizeof(char)*6) == 0) ? _switch : 0;
-    result += (memcmp(charWindow, &"typedef", sizeof(char)*7) == 0) ? _typedef : 0;
-    result += (memcmp(charWindow, &"union", sizeof(char)*5) == 0) ? _union : 0;
-    result += (memcmp(charWindow, &"unsigned", sizeof(char)*8) == 0) ? _unsigned : 0;
-    result += (memcmp(charWindow, &"void", sizeof(char)*4) == 0) ? _void : 0;
-    result += (memcmp(charWindow, &"volatile", sizeof(char)*8) == 0) ? _volatile : 0;
-    result += (memcmp(charWindow, &"while", sizeof(char)*5) == 0) ? _while : 0;
+/** 
+ * ========== TOKEN ==========
+ * Lexical Token Structure
+ * one of: *see definition*
+*/
+struct lexToken_s;
+    enum lexCKeyword_e; //int
+    struct lexIdentifier_s;
+    struct lexConstant_s;
+    struct lexStringLiteral_s;
+    enum lexPunctuator_s; //int
 
-    return result;
-}
+struct lexToken_s {
+    enum lexCKeyword_e keyWord;
 
-// might want to move span to a different file.
-span_t *spanInit(unsigned char *data, unsigned int length) {
-    span_t *span = (span_t *)malloc(sizeof(span_t));
-    if (span == NULL) {
-        return NULL;
-    }
+};
 
-    span->bp = data;
-    span->left = data;
-    span->right = data;
-    span->end = data + length;
-    return span;
-}
+/**
+ * Lexical preprocessing token.
+ * This is unimplemented. The preprocessor will be rather
+ * primitive as I do not like function macros.
+ * TODO: implement the preprocessor.
+*/
+struct preprocessingToken;
+    // unimplemented.
 
-BOOL span_popU8(span_t* span, unsigned char* U8) {
-    if (U8 == NULL || span->right >= span->end) {
-        return FALSE;
-    }
 
-    *U8 = *span->right++;
-    return TRUE;
-}
-
-BOOL span_peekU8(span_t* span, unsigned char* U8) {
-    if (U8 == NULL || span->right >= span->end) {
-        return FALSE;
-    }
-    unsigned char *temp = span->right;
-    *U8 = *temp;
-    return TRUE;
-}
-
-BOOL span_advanceU8(span_t* span) {
-    if (span->right >= span->end) {
-        return FALSE;
-    }
-    span->right++;
-    return TRUE;
-}
-
-void charScanner(span_t *fSpan) {
-    char peekChar;
-    fSpan->left = fSpan->right;
-    while(span_peekU8(fSpan, &peekChar)) {
-
-        if (isSeperator(peekChar) || isOperator(peekChar) ||  isspace(peekChar)) {
-
-            Keyword kw = getKeyword(fSpan);
-            if (kw != 0) {
-                printf("Keyword %d\n", kw);
-                return;
-            }
-
-            // else identifier
-            char *charSpanDEBUG = malloc(sizeof(char)*1024);
-            // THIS IS FOR DEBUGGING. REMOVE SOON.
-            int windowSize = (fSpan->right - fSpan->left)/ sizeof(char);
-            memcpy(charSpanDEBUG, fSpan->left, windowSize); // unsafe
-            charSpanDEBUG[windowSize] = '\0';
-            printf("Identifier: _%s_\n", charSpanDEBUG);
-            free(charSpanDEBUG);
-            return;
-        }
-        if (!span_advanceU8(fSpan)) {
-            return;
-        }
-    }
-}
-
-void numScanner() {
-    return;
-}
-
-void lexScanner(rdBuf *file) {
-    // our view window of the character stream.
-    span_t *fSpan = spanInit(file->fileContents, file->size);
-    if (fSpan == NULL) {
-        exit(0);
-    }
-
-    // lexical tokens
-    DynamicArray *tokenStream = (DynamicArray *)createDynamicArray(sizeof(lexToken));
-    if (!tokenStream) {
-        exit(0);
-    }
-
-    char peekChar = ' ';
-    char popChar = ' ';
+/**
+ * ========== Keywords ==========
+ * Lexical keywords defined in the C language grammar.
+ * if set to empty = 0, then this is unused.
+ * Also includes modern keywords which are not currently planned to be 
+ * implemented.
+ * TODO: implement logic for newer key words.
+*/
+typedef enum lexCKeyword_e {
+    // sentinal
+    empty_kw = 0, 
     
-    // we will peek the next character to decide the state of the machine during the loop.
-    // and will when eaching the end of the loop.
-    while(span_peekU8(fSpan, &peekChar)) {
-        
-        // We will have a keyword or identifier. Transfer control to the charScanner.
-        if (isalpha(peekChar)) {
-            charScanner(fSpan);
-            fSpan->left = fSpan->right; 
-        }
-        // we will have a NUMERIC literal.
-        else if (isdigit(peekChar)) {
-            span_popU8(fSpan, &popChar);
-            printf("digit UNIMPLEMENTED %c\n", popChar);
-            fSpan->left = fSpan->right;
-        }
-        else if (isspace(peekChar)) {
-            span_popU8(fSpan, &popChar);
-            fSpan->left = fSpan->right;
-        }
-        // note if it is a "" we want to treat it differently.
-        else if (isSeperator(peekChar)) {
-            span_popU8(fSpan, &popChar);
-            printf("Seperator %c\n", popChar);
-            fSpan->left = fSpan->right;
-        }
-        // note some operators are special eg ==
-        else if (isOperator(peekChar)) {
-            span_popU8(fSpan, &popChar);
-            printf("Operator %c\n", popChar);
-            fSpan->left = fSpan->right;
-        }
-    }
-    printf("finished!");
-}
+    // standard key words.
+    auto_kw, break_kw, case_kw, char_kw, const_kw, continue_kw, default_kw,
+    do_kw, double_kw, else_kw, enum_kw, extern_kw, float_kw, for_kw, goto_kw,
+    if_kw, inline_kw, int_kw, long_kw, register_kw, restrict_kw, return_kw,
+    short_kw, signed_kw, sizeof_kw, static_kw, struct_kw, switch_kw, typedef_kw,
+    union_kw, unsigned_kw, void_kw, volatile_kw, while_kw,
+    
+    // microsoft specific key words.
+    _Alignas_kw, _Alignof_kw, _Atomic_kw, _Bool_kw, _Complex_kw, _Generic_kw,
+    _Imaginary_kw, _Noreturn_kw, _Static_assert_kw, _Thread_local_kw,
+} lexKW_t;
+
+
+/**
+ * ========== Identifiers ==========
+ * Lex identifier 
+*/
+struct lexIdentifier_s;
+    struct lexIdentifierNonDigit_s;
+    // identifier + identifier-nondigit $recursve def$
+    // identifier + digit $recursive def$
+
+//*Lex: token/identifier/identifier-nondigit/universal-character-name
+struct lexUniversalCharacterName_s;
+
+typedef struct lexIdentifier_s {
+    char *identifier;
+
+} lexIdentifier_t;
+ 
+/**
+ * Lex identifier-nondigit
+ *      nondigit
+ *      universal-character name
+*/
+
+/**
+ * Lex nondigit: one of
+ * [aA-zZ] | _
+*/
+
+/**
+ * Lex digit: one of
+ * [0-9]
+*/
+    
+/**
+ * Lex universal-character-name
+ *      \u hex-quad
+ *      \U hex-quad hex-quad
+*/
+struct lexHexQuad_s;
+
+typedef struct lexUniversalCharacterName_s {
+    struct lexHexQuad_s *quad;
+    struct lexHexQuad_s *quad2;
+} lexUniCharName;
+
+/**
+ * Lex hex-quad 
+*/
+// TODO: implement hexidecimal digits.
+typedef struct lexHexQuad_s {
+    void *a;
+    // hexadecimal-digit ^ 4
+} lexHexQuad;
+
+/**
+ *  ========== Constants ==========
+ * Lex constant
+ *      integer-constant
+ *      floating-constant
+ *      enumeration-constant
+ *      character-constant
+*/
+typedef struct lexConstant_s {
+    unsigned long long uintConst;
+    signed long long intConst;
+    double ufloatConst;
+    char *characterConstant;
+    
+} lexConst_t;
+
+/**
+ * integer-constant:
+ *      decimal-constant (integer-suffix)
+ *      binary-constan-1 (integer-suffix)
+ *      octal-constant (integer-suffix)
+ *      hexadecimal-constant (integer-suffix)
+ * 
+ * decimal-constant:
+ *      nonzero-digit
+ *      decimal-constant digit $see above for digit def.
+ * 
+ * binary-constant-1:
+ *      binary-prefix binary-digit
+ *      binary-constant binary-digit $recursive definition$
+ * 
+ * binary-prefix: $one of$
+ *      0b || 0B
+ * 
+ * binary-digit: $one of$
+ *      0 | 1
+ * 
+ * octal-constant:
+ *      0
+ *      octal-constant octal-digit
+ * 
+ * hexadecimal-constant:
+ *      hexadecimal-prefix hexidecimal-digit
+ *      hexadecimal-constant hexadecimal-digit
+ * 
+ * hexadecimal-prefix: $one of$
+ *      0x 0X
+ * 
+ * nonzero-digit: 
+ *      [1-9]
+ * 
+ * octal-digit:
+ *      [0-7]
+ * 
+ * hexadecimal-digit: &one of&
+ *      [0-9]U[a-f]U[A-F]
+ * 
+ * integer-suffix:
+ *      unsigned-suffix (long-suffix)
+ *      unsigned-suffix (long-long-suffix)
+ *      long-suffix (unsigned-suffix)
+ *      long-long-suffix (unsigned-suffix)
+ * 
+ * unsigned-suffix: $one of$
+ *      u U
+ * 
+ * long-suffix: one of
+ *      l L
+ * 
+ * long-long-suffix: one of
+ *      ll LL 
+ *
+ * 
+*/
+ 
+/**
+ * floating-constant:
+        decimal-floating-constant
+        hexadecimal-floating-constant
+
+decimal-floating-constant:
+    fractional-constant (exponent-part) (floating-suffix)
+    digit-sequence exponent-part (floating-suffix)
+
+hexadecimal-floating-constant:
+    hexadecimal-prefix hexadecimal-fractional-constant (binary-exponent-part) (floating-suffix)
+    hexadecimal-prefix hexadecimal-digit-sequence binary-exponent-part (floating-suffix)
+
+fractional-constant:
+    (digit-sequence) . digit-sequence
+    digit-sequence .
+
+exponent-part:
+    e (sign) digit-sequence
+    E (sign) digit-sequence
+
+sign: one of
+    + -
+
+digit-sequence:
+    digit
+    digit-sequence digit
+
+hexadecimal-fractional-constant:
+    (hexadecimal-digit-sequence) . hexadecimal-digit-sequence
+    hexadecimal-digit-sequence .
+
+binary-exponent-part:
+    p (sign) digit-sequence
+    P (sign) digit-sequence
+
+hexadecimal-digit-sequence:
+    hexadecimal-digit
+    hexadecimal-digit-sequence hexadecimal-digit
+
+floating-suffix: one of
+    f l F L
+
+enumeration-constant:
+    identifier
+
+character-constant:
+    ' c-char-sequence '
+    L' c-char-sequence '
+
+c-char-sequence:
+    c-char
+    c-char-sequence c-char
+
+c-char:
+    Any member of the source character set except the single quotation mark ('), backslash (\), or new-line character
+    
+    escape-sequence
+
+escape-sequence:
+    simple-escape-sequence
+    octal-escape-sequence
+    hexadecimal-escape-sequence
+    universal-character-name $see above defintion$
+
+simple-escape-sequence: one of
+    \a \b \f \n \r \t \v
+    \' \" \\ \?
+
+octal-escape-sequence:
+    \ octal-digit
+    \ octal-digit octal-digit
+    \ octal-digit octal-digit octal-digit
+
+hexadecimal-escape-sequence:
+    \x hexadecimal-digit
+    hexadecimal-escape-sequence hexadecimal-digit
+    */
+
+/**
+ * ========== String literal ==========
+ * string-literal:
+    encoding-prefix " s-char-sequenceopt "
+
+ encoding-prefix:
+    u8
+    u
+    U
+    L
+
+s-char-sequence:
+    s-char
+    s-char-sequence s-char
+
+s-char:
+    any member of the source character set except the double-quotation mark ("), backslash (\), or new-line character
+    escape-sequence
+*/
+typedef struct lexStringLiteral_s {
+    char *stringLiteral;
+} lexStrLiteral_t;
+
+/**
+ * ========== Punctuators ==========
+*/
+typedef enum lexPunctuator_s {
+    empty_op = 0,
+    _oSquare, _cSquare, _oParen, _cParen, _oCurly, _cCurcly, _dot, _arrow,
+    _increment, _decrement, _bwAnd, _multiply, _plus, _minus, _tilda, /*TILDA?*/ _negation,
+    _divide, _modulo, _bsLeft, _bsRight, _logicalLess, _logicalGreater,
+    _logicalLessEqual, _logicalGreaterEqual, _logicalEqual, _logicalNotEqual, 
+    _Raise, /*RAISE?*/ _bwOr, _logicalAnd, _logicalOr, _ternary, _colon,
+    _semiColon, _ellipses /*really?*/, _assignment, _multiplyAssign, _divideAssign,
+    _moduloAssign, _plusAssign, _minusAssign, _bsLeftAssign, _bsRightAssign, 
+    _bwAndAssign, _tildaAssign, _bwOrAssign, _comma, _hashtag, _doubleHashTag,
+    _whack1, _whack2, _whack3, _whack4, _whack5, _whack6 /*<: :> <% %> %: %:%:*/
+} lexPunc_t;
 
 int main() {
-    rdBuf *file = readFile("test.txt");
-
-    lexScanner(file);
+    printf("%d", empty_op);
     return 0;
 }
+
+#endif
---
+++ lex2.h
@@ -0,0 +1,84 @@
+#ifndef LEX2_H_
+#define LEX2_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include "readFile.c"
+#include "Util/DynamicArray.c"
+
+#define BOOL int
+#define FALSE 0
+#define TRUE 1
+
+typedef enum tokenName {
+    tknInit = 0,
+    tknListen = 1,
+    tknDigitListen = 2,
+    tknSeperator = 32,
+    tknKeyword = 64,
+    tknIdentifier = 128,
+    tknLiteral = 512,
+    tknOperator = 256,
+    tknComment = 1024
+} tokenName;
+
+typedef struct lexToken {
+    tokenName name;
+    char *value;
+
+} lexToken;
+
+typedef struct span_s {
+    unsigned char* bp;
+    unsigned char* left;
+    unsigned char* right;
+    unsigned char* end;
+
+} span_t;
+
+typedef enum Keyword {
+    _auto             = 32, 
+    _break            = 1,
+    _case             = 2,
+    _char             = 3,
+    _const            = 4,
+    _continue         = 5,
+    _default          = 6,
+    _do               = 7,
+    _double           = 8,
+    _else             = 9,
+    _enum             = 10,
+    _extern           = 11,
+    _float            = 12,
+    _for              = 13,
+    _goto             = 14,
+    _if               = 15,
+    _int              = 16,
+    _long             = 17,
+    _register         = 18,
+    _return           = 19,
+    _short            = 20,
+    _signed           = 21,
+    _sizeof           = 22,
+    _static           = 23,
+    _struct           = 24,
+    _switch           = 25,
+    _typedef          = 26,
+    _union            = 27,
+    _unsigned         = 28,
+    _void             = 29,
+    _volatile         = 30,
+    _while            = 31,
+} Keyword;
+
+void lexScanner__OLD(rdBuf *file);
+void lexScanner(rdBuf *file);
+BOOL isOperator(char value);
+BOOL isSeperator(char value);
+span_t *spanInit(unsigned char *data, unsigned int length);
+BOOL span_popU8(span_t* span, unsigned char* U8);
+
+
+#endif
